59ecdbcc9892fa7eef939a075a9cd6a5b7e64b5f
xiyv <2503861416@qq.com>
2025-05-16 11:28:59 +0800

    增加404页面

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: import('../views/Home.vue'),
-    },
-    {
-      path: '/about', name: 'about', component: () => import('../views/Home.vue'),
-    },
+    {path: '/', component: import('../views/Home.vue'),},
+    {path: '/about', component: import('../views/About.vue'),},
+    {path: '/notFound', component: import('../views/404.vue'),},
+    {path: '/:pathMatch(.*)', redirect: '/notFound'},
   ],
 })
 
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+
+<template>
+<div>
+  这是关于的页面
+</div>
+</template>
